c17bd355ec5224985e73045064f6e0bcb8cd0fb4
safwatnusrat <safwatn89@gmail.com>
2025-01-26 00:19:42 +0600

    changed the email

--- components/Home/Header.jsx
+++ components/Home/Header.jsx
@@ -1,40 +1,60 @@
-import { View, Text ,StyleSheet} from 'react-native';
+import { View, Text, StyleSheet } from 'react-native';
 import React, { useEffect, useState } from 'react';
-import { onAuthStateChanged, getAuth } from "firebase/auth";
+import { getAuth } from 'firebase/auth';
+import { getFirestore, doc, getDoc } from 'firebase/firestore';
 
 const Header = () => {
-  const [user, setUser] = useState(null);
-  auth=getAuth();
+  const [userData, setUserData] = useState(null);
+  const auth = getAuth();
+  const db = getFirestore();
 
   useEffect(() => {
+    const fetchUserData = async () => {
+      const currentUser = auth.currentUser;
+      if (currentUser) {
+        try {
+          const userDocRef = doc(db, 'users', currentUser.uid);
+          const userSnap = await getDoc(userDocRef);
+
+          if (userSnap.exists()) {
+            setUserData(userSnap.data());
+          } else {
+            console.log('No such user document!');
+          }
+        } catch (error) {
+          console.error('Error fetching user data:', error);
+        }
+      }
+    };
+
     const unsubscribe = auth.onAuthStateChanged((user) => {
       if (user) {
-        setUser(user);
+        fetchUserData();
       } else {
-        setUser(null);
+        setUserData(null);
       }
     });
 
-    return () => unsubscribe();  // Clean up the listener when component unmounts
+    return () => unsubscribe(); // Cleanup function
   }, []);
 
   return (
     <View>
       <Text style={styles.welcomeText}>Welcome,</Text>
-      {user ? <Text style={styles.userinfo} >{user.email}</Text> : <Text>Loading...</Text>}
+      {userData ? <Text style={styles.userinfo}>{userData.name}</Text> : <Text>Loading...</Text>}
     </View>
   );
 };
 
 export default Header;
 
-const styles=StyleSheet.create({
-    welcomeText: {
-        fontFamily: 'outfit-regular',
-        fontSize: 18,
-      },
-      userinfo: {
-        fontSize: 18,
-        fontFamily: 'outfit-medium',
-      },
-})
+const styles = StyleSheet.create({
+  welcomeText: {
+    fontFamily: 'outfit-regular',
+    fontSize: 18,
+  },
+  userinfo: {
+    fontSize: 18,
+    fontFamily: 'outfit-medium',
+  },
+});
